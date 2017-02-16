fac980dbf7da4abd19638f34ab2553776e064a81
WyldeCat <skan1543@gmail.com>
2017-02-16 13:27:05 +0900

    Add codec

---
+++ lib/char_codec/include/char_codec.hpp
@@ -0,0 +1,18 @@
+#include <string>
+
+// TODO : make encoder
+// TODO : make trie_node
+// TODO : apply json
+// TODO : deal with redis
+// TODO : enforce find algorithm
+
+// 자음 , 모음, 자+모 를.. 다른 글자들에 맞춰주자
+
+class char_codec
+{
+public:
+  static std::string encode(std::string &str)
+  {
+  }
+
+};
